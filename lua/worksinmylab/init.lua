871831da29800cc8bb888017cb68cea16785fb1e
Sebastian Mueller (WorksInMyLab) <worksinmylab@mailbox.org>
2025-11-21 02:50:00 +0100

    Fixed format on save

--- lua/worksinmylab/init.lua
+++ lua/worksinmylab/init.lua
@@ -1,3 +1,12 @@
 require("worksinmylab.remap")
 require("worksinmylab.lazy_init")
 require("worksinmylab.set")
+
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd("BufWritePre", {
+    pattern = "*",
+    callback = function(args)
+        require("conform").format({ bufnr = args.buf })
+    end,
+})
