a68efa12d059948339c3282361126f5a821f385e
Sebastian Mueller (WorksInMyLab) <worksinmylab@mailbox.org>
2025-11-28 00:38:01 +0100

    Added live grep

--- lua/worksinmylab/plugins/telescope.lua
+++ lua/worksinmylab/plugins/telescope.lua
@@ -3,11 +3,23 @@ return {
     tag = "0.1.8",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
-        require("telescope").setup({})
+        require("telescope").setup({
+            pickers = {
+                find_files = {
+                    theme = "ivy",
+                },
+            },
+        })
 
         local builtin = require("telescope.builtin")
         -- find files
         vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+        -- edit Neovim config
+        vim.keymap.set("n", "<leader>en", function()
+            builtin.find_files({
+                cwd = vim.fn.stdpath("config"),
+            })
+        end)
         -- find files in current git repo
         vim.keymap.set("n", "<C-p>", builtin.git_files, {})
         -- find word
@@ -20,11 +32,9 @@ return {
             local word = vim.fn.expand("<cWORD>")
             builtin.grep_string({ search = word })
         end)
-        -- grep string
-        vim.keymap.set("n", "<leader>ps", function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        end)
         -- help tags
         vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
+
+        require("worksinmylab.plugins.telescope.livegrep").setup()
     end,
 }
