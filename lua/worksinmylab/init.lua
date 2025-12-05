1b3a7aac7e8489a49e704758e85503aa42d4762f
Sebastian Mueller (WorksInMyLab) <worksinmylab@mailbox.org>
2025-12-05 02:45:49 +0100

    Added highlight when yanking

--- lua/worksinmylab/init.lua
+++ lua/worksinmylab/init.lua
@@ -4,9 +4,27 @@ require("worksinmylab.remap")
 
 local autocmd = vim.api.nvim_create_autocmd
 
+-- trigger format on save
 autocmd("BufWritePre", {
+    desc = "Format on save",
+    group = vim.api.nvim_create_augroup(
+        "worksinmylab-format-save",
+        { clear = true }
+    ),
     pattern = "*",
     callback = function(args)
         require("conform").format({ bufnr = args.buf })
+    end,
+})
+
+-- highlight when yank
+autocmd("TextYankPost", {
+    desc = "Highlight when yanking (copying) text",
+    group = vim.api.nvim_create_augroup(
+        "worksinmylab-highlight-yank",
+        { clear = true }
+    ),
+    callback = function()
+        vim.highlight.on_yank()
     end,
 })
